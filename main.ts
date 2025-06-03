f4796ca9ff497d8cbca9a1bb81c952a05d409ec4
Franco Logozzo <franco.logozzo@gmail.com>
2025-06-02 22:08:54 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+player.onChat("run", function () {
+	
+})
+blocks.fill(
+TNT,
+pos(0, 10, 5),
+pos(-4, 10, -5),
+FillOperation.Replace
+)
+blocks.fill(
+REDSTONE_BLOCK,
+pos(150, 15, 5),
+pos(-4, 11, -5),
+FillOperation.Replace
+)
